e8fbcfb0fafbf2be35a4259cb466b9b349422e81
Belle <belle_xu@apple.com>
2022-05-21 20:16:17 +0800

    js

---
+++ index.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./index.css">
+</head>
+<body>
+    <div>123</div>
+    <h1>这是大标题</h1>
+    <script src="./index.js"></script>
+</body>
+</html>
